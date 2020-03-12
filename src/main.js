af61dca4fc7085e0dac6faf6be0388978b9146a3
Eclair-F <56910441+Eclair-F@users.noreply.github.com>
2020-03-12 16:35:14 +0800

    用户管理

--- src/main.js
+++ src/main.js
@@ -3,11 +3,47 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+// 导入全局css样式表
+import './assets/css/global.css'
+// 导入字体图标样式库
+import './assets/fonts/iconfont.css'
+// 导入axios,并配置
+import axios from 'axios'
+//配置cookies
+import Cookies from 'vue-cookies'
+//配置axios
+axios.defaults.baseURL = "http://127.0.0.1:8081/";
+//axios.defaults.withCredentials = true;
+axios.interceptors.request.use(config => {
+	// 在发起axios的时候展示进度条
+	//  NProgress.start()
+	var token=window.sessionStorage.getItem('token');
+	if(token!=null)
+	{
+	config.headers.Authentication = token;
+	}
+	//console.log(config)
+	return config;
+})
+// axios.interceptors.response.use(config => {
+//       NProgress.done()
+//       return config
+// })
+Vue.prototype.axios = axios;
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+// // 配置cookie
+// import cookie from 'vue-cookies'
+// Vue.prototype.$cookie = cookie;
+
+//配置cookies
+Vue.use(Cookies);
+// 30 day after, expire
+Vue.$cookies.config('3d');
+
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Login from "../components/Login.vue"
+import Home from "../components/Home.vue"
+import Admin from "../components/Admin.vue"
+import Test from "../components/test.vue"
+import Welcome from "../components/children/Welcome.vue"
+import User from "../components/children/User.vue"
 Vue.use(VueRouter)
 
-const routes = [
+
+const routes = [{
+		path: "/login",
+		component: Login
+	}, {
+		path: "/home",
+		component: Home
+	},
+	{
+		path: "/admin",
+		component: Admin,
+		redirect: "/welcome",
+		children: [{
+				path: "/welcome",
+				component: Welcome
+			},
+			{
+				path: '/user',
+				component: User
+			}
+		]
+	},
+	{
+		path: "/test",
+		component: Test
+	}
 
 ]
 
 const router = new VueRouter({
-  routes
+	routes
 })
 
 export default router
